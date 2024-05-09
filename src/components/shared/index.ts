2bcec5174a7a3be68db647d8d7c89f1900abd6f4
Daniel Montillo <Danielmontillo55@gmail.com>
2024-05-09 15:20:31 -0500

    Correct

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,4 +1,3 @@
 export { Inputs } from "@/components/shared/Inputs";
 export { ButtonXR } from "@/components/shared/ButtonXR";
 export { HandsMap } from "@/components/shared/collidersMap";
-export { Cubes } from "@/components/shared/objectDynamic"
