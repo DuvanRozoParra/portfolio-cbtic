86f79d7afa8e3addc24c52c0ff684603ff122bbf
DuvanRozoParra <dduvanrozo@gmail.com>
2024-07-18 17:01:23 -0500

    feat: add interaction 3d

--- src/components/LawFaraday/LawFaraday.tsx
+++ src/components/LawFaraday/LawFaraday.tsx
@@ -1,28 +1,67 @@
 "use client";
 
-import { ViewVR } from "@/layout/ViewVR";
-import { LawFaradayModel } from "@/models/LawFaraday";
-import { CubesDynamic, TableCubesDymanic } from "@/components/shared/objectDynamic";
-import * as THREE from "three";
-import { useBox } from "@react-three/cannon";
+import { Iman, Aspire, FieldAspire } from "@/models/LawFaraday";
+import { g } from "math";
+import React, { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+} from "@react-three/postprocessing";
+import { Slider } from "@ui/slider";
+import { useMove } from "@store";
 
 export const LawFaradayPage = () => {
+  const refSlider = useRef<number>(0);
+
   return (
-    <ViewVR>
-      <LawFaradayModel
-        position={[0, 0.2, -1]}
-        rotation={[0, (-90 * Math.PI) / 180, 0]}
-        scale={0.48}
-      /> 
-      <group >
-        {Array.from({ length: 6 }).map((item, index) => ( // [0,0.5,2]
-          <CubesDynamic key={"cube" + index} position={[0,0.8 + 0.2 * index,1.2]}  />
-        ))}
-      </group>
-      <TableCubesDymanic/>
-    </ViewVR>
+    <>
+      <Canvas shadows>
+        <group
+          scale={0.002}
+          position={[0.5, 0.3, 3]}
+          rotation={[g(35), g(115), 0]}
+        >
+          <Iman position={[0, 0, 0.25]} />
+          <Aspire />
+          <FieldAspire />
+        </group>
+        {/* <OrbitControls /> */}
+        <Environment preset="lobby" blur={1} />
+        <EffectComposer>
+          <Bloom
+            luminanceThreshold={0}
+            luminanceSmoothing={0.9}
+            height={300}
+            intensity={0.4}
+          />
+          <DepthOfField
+            focusDistance={0}
+            focalLength={0.02}
+            bokehScale={2}
+            height={480}
+          />
+          {/* <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+        </EffectComposer>
+      </Canvas>
+      <section className="absolute bottom-10 left-1/2 w-[200px] bg-gray-400 h-10 flex items-center justify-center rounded-full px-4">
+        <Slider
+          onValueChange={(value) => {
+            useMove.getState().setX(value[0]);
+          }}
+          defaultValue={[0]}
+          max={200}
+          step={1}
+          min={1}
+          className="w-[200px]"
+        />
+      </section>
+    </>
   );
-};// 2.3,-0.8,0
+}; // 2.3,-0.8,0
 
 /*
     <>
