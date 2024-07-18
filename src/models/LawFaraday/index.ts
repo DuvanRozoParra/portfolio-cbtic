92fa960945a3cd3305e4bbec17225dfbd75daa47
DuvanRozoParra <dduvanrozo@gmail.com>
2024-07-18 17:00:43 -0500

    feat: export components

--- src/models/LawFaraday/index.ts
+++ src/models/LawFaraday/index.ts
@@ -1,2 +1,5 @@
-export { LawFaradayModel } from "@/models/LawFaraday/DesktopAndInteractividad"
-export { BoxCollider } from "@/models/LawFaraday/BoxCollider"
+export { LawFaradayModel } from "@/models/LawFaraday/DesktopAndInteractividad";
+export { BoxCollider } from "@/models/LawFaraday/BoxCollider";
+export { Model as Iman } from "@/models/LawFaraday/Iman";
+export { Model as Aspire } from "@/models/LawFaraday/Aspire";
+export { Model as FieldAspire } from "@/models/LawFaraday/FieldAspire";
